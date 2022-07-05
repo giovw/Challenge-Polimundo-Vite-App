5668b27b2d20d0ff1d863df5fafb9a2b2f61be54
Giovanny <giov_w@hotmail.com>
2022-07-05 14:00:04 -0500

    Initial Vuejs 2 + ViteJs

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+	<div id="app">
+		<component :is="layout">
+			<router-view />
+		</component>
+	</div>
+</template>
+
+<script>
+	export default {
+		computed: {
+			layout() {
+				return `layout-${this.$route.meta.layout || 'default'}`
+			}
+		}
+	}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="banner-top px-5">
+		<h1>{{ msg }}</h1>
+		<h4>Mas asientos y mejores precios.</h4>
+		<b-button variant="light" class="mt-5 px-4" pill>Comprar ahora</b-button>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: {
+			msg: String
+		}
+	}
+</script>
+
+<style scoped>
+.banner-top {
+  color: #ffffff;
+}
+</style>
